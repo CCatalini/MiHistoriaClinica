e7e0e34957b5ff5856b9e5689e3614b3ea91d0d2
CCatalini <camila.catalini@ing.austral.edu.ar>
2023-03-08 12:20:54 -0300

    Initial commit

---
+++ src/test/java/com/example/MiHistoriaClinica/MiHistoriaClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiHistoriaClinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiHistoriaClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
