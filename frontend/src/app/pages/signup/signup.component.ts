bc14f523d269cfc0880067319d293a3fb2bcd092
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2023-04-05 18:54:36 -0300

    Integración front login con back

--- frontend/src/app/pages/signup/signup.component.ts
+++ frontend/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-signup',
@@ -6,8 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit{
-  constructor() {}
+
+    public user = {
+        name: '',
+        surname: '',
+        password: '',
+        DNI: '',
+        date_of_birth: '',
+        email: ''
+    }
+
+    constructor(private userService:UserService){
+
+    }
+
     ngOnInit():void{
+    }
+
+    formSubmit(){
+      console.log(this.user);
+      if(this.user.DNI == '' || this.user.DNI == null){
+          alert('El nombre de usuario es requerido');
+          return;
+      }
+
+      this.userService.añadirUsuario(this.user).subscribe(
+          (data) => {
+              console.log(data);
+              alert('Usuario guardado con exito');
+          },(error) => {
+              console.log(error);
+              alert('Ha ocurrido un eror en el sistema');
+          }
+      )
+
 
     }
 }
--- src/main/java/com/example/MiHistoriaClinica/controller/UserController.java
+++ src/main/java/com/example/MiHistoriaClinica/controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
