f5c92df99dc46c37e23b8eabb10c3d4655f08985
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2023-04-05 10:27:00 -0300

    Frontend cambios 2

---
+++ src/main/frontend/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    //RegistrarPacienteComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent
+  ],
+    imports: [
+        BrowserModule,
+        RouterModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
