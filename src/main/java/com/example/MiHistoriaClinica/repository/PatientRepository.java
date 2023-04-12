35cdbc211d348d3ce9209ef7017674105c7fdf69
cami <camila.catalini@ing.austral.edu.ar>
2023-04-12 14:46:47 -0300

    abm doctor y paciente, registro doctor y paciente

--- src/main/java/com/example/MiHistoriaClinica/repository/PatientRepository.java
+++ src/main/java/com/example/MiHistoriaClinica/repository/PatientRepository.java
@@ -5,6 +5,8 @@ import com.example.MiHistoriaClinica.model.PatientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// todo
+
 @Repository
 public interface PatientRepository extends JpaRepository<PatientModel, Long> {
 
