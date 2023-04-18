9c764f70fddd258b67d259b79fc177cc476abc70
cami <camila.catalini@ing.austral.edu.ar>
2023-04-18 15:59:05 -0300

    abm doctor y paciente, registro doctor y paciente

--- src/main/java/com/example/MiHistoriaClinica/controller/DoctorController.java
+++ src/main/java/com/example/MiHistoriaClinica/controller/DoctorController.java
@@ -4,6 +4,7 @@ package com.example.MiHistoriaClinica.controller;
 import com.example.MiHistoriaClinica.exception.ResourceNotFoundException;
 import com.example.MiHistoriaClinica.model.DoctorModel;
 import com.example.MiHistoriaClinica.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 @CrossOrigin("*")
 public class DoctorController {
 
-    DoctorRepository doctorRepository;
+   @Autowired  private DoctorRepository doctorRepository;
 
     @PostMapping()
     public DoctorModel createDoctor(@RequestBody DoctorModel doctor) {
