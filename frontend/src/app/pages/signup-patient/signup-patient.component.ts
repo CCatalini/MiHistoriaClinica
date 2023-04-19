d60ae2b911a519cb1438fcdc17c85e4d9c049fb2
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2023-04-19 17:34:26 -0300

    Modificacion de alertas de registro

--- frontend/src/app/pages/signup-patient/signup-patient.component.ts
+++ frontend/src/app/pages/signup-patient/signup-patient.component.ts
@@ -29,25 +29,18 @@ export class SignupPatientComponent implements OnInit{
     formSubmit(){
       console.log(this.user);
       if(this.user.dni == '' || this.user.dni == null){
-          this.snack.open("El DNI es requerido", "Aceptar",{
-              duration: 3000,
-              verticalPosition: 'top',
-              horizontalPosition: 'right'
-          });
+          Swal.fire('Ingrese su DNI', 'El DNI es requisito para registrarse como paciente.', 'warning');
           return;
       }
 
       this.userService.añadirUsuario(this.user).subscribe(
           (data) => {
               console.log(data);
-              Swal.fire('Usuario guardado', 'Usuario registrado con éxito en el sistema', 'success');
+              Swal.fire('Usuario guardado', 'Usuario registrado con éxito en el sistema.', 'success');
           },(error) => {
               console.log(error);
-              this.snack.open("Ha ocurrido un error en el sistema", "Aceptar",{
-                  duration: 3000,
-                  verticalPosition: 'top',
-                  horizontalPosition: 'right'
-              });          }
+              Swal.fire('Error', 'Falta completar algún campo o existen datos repetidos.', 'error');
+          }
       )
 
 
