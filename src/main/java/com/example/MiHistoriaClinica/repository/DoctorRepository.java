7a31eccdd03daf9d80c30d12e66702827eded238
cami <camila.catalini@ing.austral.edu.ar>
2023-04-20 14:25:57 -0300

    tablas y repositorios para medicamentos y laboratorios

--- src/main/java/com/example/MiHistoriaClinica/repository/DoctorRepository.java
+++ src/main/java/com/example/MiHistoriaClinica/repository/DoctorRepository.java
@@ -4,4 +4,6 @@ import com.example.MiHistoriaClinica.model.DoctorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<DoctorModel, Long> {
+
+    DoctorModel findByDniAndPassword(Long dni, String password);
 }
---
+++ src/main/java/com/example/MiHistoriaClinica/repository/MedicineRepository.java
@@ -0,0 +1,7 @@
+package com.example.MiHistoriaClinica.repository;
+
+import com.example.MiHistoriaClinica.model.MedicineModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicineRepository extends JpaRepository<MedicineModel, Long> {
+}
