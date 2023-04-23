f28b809f32e4d35ec86b9fe1c2f21f34691b6725
cami <camila.catalini@ing.austral.edu.ar>
2023-04-23 00:52:18 -0300

    paquetitos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,13 +12,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {HttpClientModule} from "@angular/common/http";
 import {HomeComponent} from "./pages/home/home.component";
-import {SignupMedicComponent} from './pages/signup-medic/signup-medic.component';
-import {SignupPatientComponent} from './pages/signup-patient/signup-patient.component';
+import {SignupMedicComponent} from './pages/medic/signup-medic/signup-medic.component';
+import {SignupPatientComponent} from './pages/patient/signup-patient/signup-patient.component';
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {AppRoutingModule} from "./app-routing.module";
-import { LoginPatientComponent } from './pages/login-patient/login-patient.component';
-import { LoginMedicComponent } from './pages/login-medic/login-medic.component';
+import { LoginPatientComponent } from './pages/patient/login-patient/login-patient.component';
+import { LoginMedicComponent } from './pages/medic/login-medic/login-medic.component';
 import { AddMedicineComponent } from './pages/alta-medicamento/add-medicine.component';
 
 @NgModule({
