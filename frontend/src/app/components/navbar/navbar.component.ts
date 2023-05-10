3e5ea2670a51d1cce349150fb6bb488f9addf36d
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2023-05-10 12:37:53 -0300

    todo ok

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit{
-  constructor() {
-  }
-  ngOnInit() : void{
-  }
+    constructor(private router: Router) { }
+
+    ngOnInit() : void{
+    }
+
+    isHomePage(): boolean {
+        return this.router.url === '/' || this.router.url==='/patient/login' || this.router.url==='/patient/signup' || this.router.url==='/medic/login' || this.router.url==='/medic/signup';
+    }
+
 }
