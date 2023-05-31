ea2c7e4d78324bf4f41fbc5035b61cc878478fdf
cami <camila.catalini@ing.austral.edu.ar>
2023-05-31 10:22:37 -0300

    lo del campo

---
+++ frontend/src/app/pages/patient/generate-link-code/generate-link-code.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GenerateLinkCodeComponent } from './generate-link-code.component';
+
+describe('GenerateLinkCodeComponent', () => {
+  let component: GenerateLinkCodeComponent;
+  let fixture: ComponentFixture<GenerateLinkCodeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GenerateLinkCodeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GenerateLinkCodeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
