b0071cc412c9caaaad8256696b65e116b94483f7
cami <camila.catalini@ing.austral.edu.ar>
2023-05-31 10:22:37 -0300

    lo del campo

---
+++ frontend/src/app/pages/patient/generate-link-code/generate-link-code.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-link-code',
+  templateUrl: './generate-link-code.component.html',
+  styleUrls: ['./generate-link-code.component.css']
+})
+export class GenerateLinkCodeComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
