2d6de1928a97aec4a4bb11d6d582716dcf90c49a
cami <camila.catalini@ing.austral.edu.ar>
2023-06-13 12:51:05 -0300

    OK postman --> create-medicine --> get-patients
    VOLVIERON A ANDARRRRR

--- src/main/java/com/example/MiHistoriaClinica/model/MedicineModel.java
+++ src/main/java/com/example/MiHistoriaClinica/model/MedicineModel.java
@@ -1,6 +1,10 @@
 package com.example.MiHistoriaClinica.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "Medicine")
@@ -9,19 +13,43 @@ public class MedicineModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
-    private Long medicine_id;
+    private Long medicineId;
 
     @Column(name="medicine")
     private String medicineName;
-
     @Column(name="active_ingredient")
     private String activeIngredient;
-
     private String lab;
-
     private String description;
-
     private String status;
+
+
+    @ManyToMany(mappedBy = "medicines")
+    @JsonBackReference
+    private List<PatientModel> patients = new ArrayList<>();
+
+
+
+    public void addPatient(PatientModel patient){
+        patients.add(patient);
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<PatientModel> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<PatientModel> patients) {
+        this.patients = patients;
+    }
 
     public String getLab() {
         return lab;
@@ -35,12 +63,12 @@ public class MedicineModel {
         this.status = status;
     }
 
-    public Long getMedicine_id() {
-        return medicine_id;
+    public Long getMedicineId() {
+        return medicineId;
     }
 
-    public void setMedicine_id(Long medicine_id) {
-        this.medicine_id = medicine_id;
+    public void setMedicineId(Long medicine_id) {
+        this.medicineId = medicine_id;
     }
 
     public String getMedicineName() {
