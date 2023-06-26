bd58e4c962cafde571cd109b0f2c9fd3d6e7ef27
cami <camila.catalini@ing.austral.edu.ar>
2023-06-26 15:00:57 -0300

    OK get-Analysis desde el paciente cambio de controlador para algunos metodos

---
+++ src/main/java/com/example/MiHistoriaClinica/service/AnalysisServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.MiHistoriaClinica.service;
+
+import com.example.MiHistoriaClinica.dto.AnalysisDTO;
+import com.example.MiHistoriaClinica.model.AnalysisModel;
+import com.example.MiHistoriaClinica.model.MedicModel;
+import com.example.MiHistoriaClinica.model.PatientModel;
+import com.example.MiHistoriaClinica.repository.AnalysisRepository;
+import com.example.MiHistoriaClinica.repository.CustomRepositoryAccess;
+import com.example.MiHistoriaClinica.repository.MedicRepository;
+import com.example.MiHistoriaClinica.repository.PatientRepository;
+import com.example.MiHistoriaClinica.service.interfaces.AnalysisService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AnalysisServiceImpl implements AnalysisService {
+
+    private final MedicRepository medicRepository;
+    private final PatientRepository patientRepository;
+    private final AnalysisRepository analysisRepository;
+    private final CustomRepositoryAccess customRepositoryAccess;
+
+    public AnalysisServiceImpl(MedicRepository medicRepository, PatientRepository patientRepository, AnalysisRepository analysisRepository, CustomRepositoryAccess customRepositoryAccess) {
+        this.medicRepository = medicRepository;
+        this.patientRepository = patientRepository;
+        this.analysisRepository = analysisRepository;
+        this.customRepositoryAccess = customRepositoryAccess;
+    }
+
+    @Override
+    public AnalysisModel createPatientAnalysis(Long medicId, String patientLinkCode, AnalysisDTO analysisDTO) {
+
+        Optional<MedicModel> medic = medicRepository.findById(medicId);
+        Optional<PatientModel> patient = patientRepository.findByLinkCode(patientLinkCode);
+
+        if (medic.isEmpty() || patient.isEmpty() ) return null;
+        else    return customRepositoryAccess.createPatientAnalysis(analysisDTO, patient);
+
+    }
+
+    @Override
+    public List<AnalysisModel> getAnalysisByPatientId(Long id) {
+        return analysisRepository.getAnalysisByPatientId(id);
+    }
+}
