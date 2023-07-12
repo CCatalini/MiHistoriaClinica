4864f7fe287b93c2dffcb671d6a86e5a20e4e38b
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2023-07-11 22:21:50 -0300

    Muestro datos del paciente al atender

--- frontend/src/app/pages/medic/attend-patient/attend-patient.component.ts
+++ frontend/src/app/pages/medic/attend-patient/attend-patient.component.ts
@@ -1,29 +1,52 @@
-
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-  selector: 'app-attend-patient',
-  templateUrl: './attend-patient.component.html',
-  styleUrls: ['./attend-patient.component.css']
+    selector: 'app-attend-patient',
+    templateUrl: './attend-patient.component.html',
+    styleUrls: ['./attend-patient.component.css']
 })
 export class AttendPatientComponent implements OnInit {
-    patientId : string = '';
-    constructor(private route:ActivatedRoute) { }
+    patientId: string = '';
+    patient: any;
+
+    constructor(
+        private route: ActivatedRoute,
+        private httpClient: HttpClient
+    ) { }
 
     ngOnInit(): void {
-        //verifico usuario
-        if (localStorage.getItem('userType') != 'MEDIC') {
+        // Verify user
+        if (localStorage.getItem('userType') !== 'MEDIC') {
             window.location.href = '/medic/login';
         } else {
             this.route.params.subscribe(params => {
                 this.patientId = params['linkCode'];
             });
+
+            // Call the method to fetch patient info
+            this.getPatientInfo();
         }
     }
 
-    setLinkCode(): void{
+    setLinkCode(): void {
         localStorage.setItem('patientLinkCode', '');
     }
 
+    getPatientInfo(): void {
+        const token = localStorage.getItem('token');
+        let headers = new HttpHeaders();
+        if (token) {
+            headers = headers.set('Authorization', "Bearer " + token);
+        }
+        this.httpClient.get<any>('http://localhost:8080/patient/get-patient-info', { headers }).subscribe(
+            (response: any) => {
+                this.patient = response;
+            },
+            (error: any) => {
+                console.error('Error fetching patient info:', error);
+            }
+        );
+    }
 }
