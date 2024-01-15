b47ebc3e39a60723ef421c6758737879c7a511bb
ccatalini <camila.catalini@ing.austral.edu.ar>
2024-01-15 18:11:25 -0300

    changes with dependency lombok

--- MiHistoriaClinica/src/main/java/com/example/MiHistoriaClinica/persistence/model/Analysis.java
+++ MiHistoriaClinica/src/main/java/com/example/MiHistoriaClinica/persistence/model/Analysis.java
@@ -2,80 +2,33 @@ package com.example.MiHistoriaClinica.persistence.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name= "Analysis")
+@Getter
+@Setter
 public class Analysis {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
     private Long analysis_id;
-
     @Column(nullable = false)
     private String name;
     private String medicalCenter;
     private String description;
     private String status;
 
-
     @ManyToMany(mappedBy = "analysis", fetch = FetchType.EAGER)
     @JsonBackReference
     private List<Patient> patients = new ArrayList<>();
-
-
-    public List<Patient> getPatients() {
-        return patients;
-    }
 
     public void addPatient(Patient patient){
         this.patients.add(patient);
     }
 
-    public void setPatients(List<Patient> patients) {
-        this.patients = patients;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Long getAnalysis_id() {
-        return analysis_id;
-    }
-
-    public void setAnalysis_id(Long medicalAnalysis_id) {
-        this.analysis_id = medicalAnalysis_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMedicalCenter() {
-        return medicalCenter;
-    }
-
-    public void setMedicalCenter(String medicalCenter) {
-        this.medicalCenter = medicalCenter;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
 
