9bfcc9ab925cc71375692d261b6569058b0a9d3a
ccatalini <camila.catalini@ing.austral.edu.ar>
2025-12-09 10:30:12 -0300

    🛟 carga de agendas medicas con scrips completa

---
+++ MiHistoriaClinica/src/main/resources/scripts-turnos/turnos_oftalmologia.sql
@@ -0,0 +1,69 @@
+-- ============================================
+-- OFTALMOLOGÍA (specialty = 8)
+-- Médicos: Facundo Ramos (35), Antonella Ponce (36), Maximiliano Navarro (37)
+-- ============================================
+
+-- Facundo Ramos (ID 35) - Lunes y Miércoles, mañana - H. Austral
+INSERT INTO turnos (available, fecha_turno, hora_turno, medic_full_name, medic_specialty, medical_center, medic_id) VALUES
+(true, '2025-12-09', '08:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-09', '08:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-09', '09:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-09', '09:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-09', '10:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-09', '10:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-11', '08:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-11', '08:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-11', '09:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-11', '09:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-11', '10:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-11', '10:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-16', '08:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-16', '08:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-16', '09:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-16', '09:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-18', '08:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-18', '08:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-18', '09:00:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35),
+(true, '2025-12-18', '09:30:00', 'Facundo Ramos', 8, 'SEDE_PRINCIPAL_HOSPITAL_AUSTRAL', 35);
+
+-- Antonella Ponce (ID 36) - Martes y Viernes, tarde - C. Champagnat
+INSERT INTO turnos (available, fecha_turno, hora_turno, medic_full_name, medic_specialty, medical_center, medic_id) VALUES
+(true, '2025-12-10', '14:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-10', '14:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-10', '15:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-10', '15:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-10', '16:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-10', '16:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-13', '14:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-13', '14:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-13', '15:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-13', '15:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-13', '16:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-13', '16:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-17', '14:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-17', '14:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-17', '15:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-17', '15:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-20', '14:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-20', '14:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-20', '15:00:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36),
+(true, '2025-12-20', '15:30:00', 'Antonella Ponce', 8, 'CENTRO_ESPECIALIDAD_CHAMPAGNAT', 36);
+
+-- Maximiliano Navarro (ID 37) - Jueves, mañana y tarde - C. Luján
+INSERT INTO turnos (available, fecha_turno, hora_turno, medic_full_name, medic_specialty, medical_center, medic_id) VALUES
+(true, '2025-12-12', '09:00:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-12', '09:30:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-12', '10:00:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-12', '10:30:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-12', '14:00:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-12', '14:30:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-12', '15:00:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-12', '15:30:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-19', '09:00:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-19', '09:30:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-19', '10:00:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-19', '10:30:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-19', '14:00:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-19', '14:30:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-19', '15:00:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37),
+(true, '2025-12-19', '15:30:00', 'Maximiliano Navarro', 8, 'CENTRO_ESPECIALIDAD_LUJAN', 37);
